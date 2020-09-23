4d71a369453f629f2c22e4cbb7037e5cefdbf5c8
Rajesh Sharma <rajesh.vbn@gmail.com>
2020-09-23 21:45:54 +0530

    fix card component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -7,15 +7,13 @@ const Card = ({ launch }) => {
 			<h6 className="card_head">{launch.mission_name}</h6>
 			<span></span>
 			<ul className="card_detail">
+				<strong>Mission ids: </strong>
 				{launch.mission_id.length > 0 && (
-					<>
-						<strong>Mission ids: </strong>
-						<ul className="card_ids">
-							{launch.mission_id.map((id) => (
-								<li key={id}>{id}</li>
-							))}
-						</ul>
-					</>
+					<ul className="card_ids">
+						{launch.mission_id.map((id) => (
+							<li key={id}>{id}</li>
+						))}
+					</ul>
 				)}
 				<li>
 					<strong>Launch Year: </strong>
@@ -27,7 +25,7 @@ const Card = ({ launch }) => {
 				</li>
 				<li>
 					<strong>Successful Landing: </strong>
-					{ launch.rocket.first_stage.cores[0].land_success + ""}
+					{launch.rocket.first_stage.cores[0].land_success + ""}
 				</li>
 			</ul>
 		</div>
